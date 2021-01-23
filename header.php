20bda6a790b481f8f54ab88e9f3ecd79e27d5000
Shikhar Tyagi <shikhart98@gmail.com>
2021-01-23 18:33:52 +0530

    make profile options

--- header.php
+++ header.php
@@ -88,10 +88,10 @@ include 'includes/dbconnection.php';
         <?php
           if(isset($_SESSION['UserID'])){
             echo'
-            <li><a href="profile.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart</a></li>
-            <li><a href="profile.php"><i class="fa fa-heart" aria-hidden="true"></i> Wishlist</a></li>
-            <li><a href="profile.php"><i class="fa fa-shopping-bag" aria-hidden="true"></i> Orders</a></li>
-            <li><a href="profile.php"><i class="fa fa-user-circle" aria-hidden="true"></i> Profile</a></li>
+            <li><a href="profile.php?cart=1"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart</a></li>
+            <li><a href="profile.php?wishlist=1"><i class="fa fa-heart" aria-hidden="true"></i> Wishlist</a></li>
+            <li><a href="profile.php?order=1"><i class="fa fa-shopping-bag" aria-hidden="true"></i> Orders</a></li>
+            <li><a href="profile.php?profile=1"><i class="fa fa-user-circle" aria-hidden="true"></i> Profile</a></li>
             <li><a href="includes/signout.php" class="rounded-pill">log out </a></li></ul>';
           }
           else {
--- profile.php
+++ profile.php
@@ -10,10 +10,18 @@
     <div class="row" style="font-family:Georgia">
         <div class="col-lg-4"></div>
        <div class="col-lg-4 col-md-4" >
-
-       <h1 class="text-center pb-3 pt-3"><span class="hm_color"><b>Welcome</b></span><br><br><br><?= $_SESSION['FirstName'] . ' ' . $_SESSION['LastName'] ?> </h1>
-
-        
+        <?php
+        if (!isset($_GET)) { ?>
+            <h1 class="text-center pb-3 pt-3"><span class="hm_color"><b>Welcome</b></span><br><br><br><?= $_SESSION['FirstName'] . ' ' . $_SESSION['LastName'] ?> </h1>
+        <?php } else if (isset($_GET['cart']) && $_GET['cart'] == '1') { ?>
+            <h1 class="text-center pb-3 pt-3"><span class="hm_color"><b><?= ucfirst($_SESSION['FirstName']) . "'s" ?> Cart</b></span></h1>
+        <?php } else if (isset($_GET['wishlist']) && $_GET['wishlist'] == '1') { ?>
+            <h1 class="text-center pb-3 pt-3"><span class="hm_color"><b><?= ucfirst($_SESSION['FirstName']) . "'s" ?> Wishlist</b></span></h1>
+        <?php } else if (isset($_GET['order']) && $_GET['order'] == '1') { ?>
+            <h1 class="text-center pb-3 pt-3"><span class="hm_color"><b><?= ucfirst($_SESSION['FirstName']) . "'s" ?> Orders</b></span></h1>
+        <?php } else {?>
+            <h1 class="text-center pb-3 pt-3"><span class="hm_color"><b>Welcome</b></span><br><br><br><?= ucfirst($_SESSION['FirstName']) . ' ' . ucfirst($_SESSION['LastName']) ?> </h1>
+        <?php }?>
        </div><!--form-->
 
 

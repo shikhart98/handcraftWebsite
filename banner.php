2cd14160cfecd92554581878cfb39b0ded3e30ef
Shikhar Tyagi <shikhart98@gmail.com>
2021-01-24 13:31:09 +0530

    remove login and buy now from banner

--- banner.php
+++ banner.php
@@ -20,12 +20,6 @@
                             <!-- LAYER NR. 2 -->
                             <div class="tp-caption Restaurant-Cursive   tp-resizeme" id="slide-3010-layer-2" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['-60','-60','-70','-70']" data-width="none" data-height="none" data-whitespace="nowrap" data-fontsize="['18','18','15','15']" data-type="text" data-responsive_offset="on" data-frames='[{"from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":700,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"opacity:0;","ease":"nothing"}]' data-textAlign="['center','center','center','center']" data-paddingtop="[0,0,0,0]" data-paddingright="[0,0,0,20]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,20]" style="z-index: 6; white-space: nowrap; line-height:26px;text-transform:left;font-family: 'Roboto', sans-serif;letter-spacing:0px;">In Craft Village, we will provide the platform to the customers to buy and sell handcraft material</div>
 
-                            <!-- LAYER NR. 3 -->
-                            <div class="tp-caption NotGeneric-SubTitle   tp-resizeme" id="slide-3045-layer-5" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['40','40','30','30']" data-width="none" data-height="none" data-whitespace="nowrap" data-type="text" data-responsive_offset="on" data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","speed":1500,"to":"o:1;","delay":2000,"ease":"Power4.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"Power2.easeInOut"}]' data-textAlign="['center','left','left','left']" data-paddingtop="[25,0,0,0]" data-paddingright="[33,0,0,0]" data-paddingbottom="[10,0,0,0]" data-paddingleft="[33,0,0,0]" style="z-index: 6; white-space: nowrap;text-transform:capitalize;letter-spacing:1px;    font-family: 'Roboto', sans-serif;">
-                                <a href="store.php" class="hm_btn" target="_blank" style="margin-right:10px;font-family: 'Roboto', sans-serif;">Buy Now</a>
-                                <a href="login.php" class="hm_btn" target="_blank">Login</a>
-                            </div>
-
                         </li>
                     </ul>
                 </div>

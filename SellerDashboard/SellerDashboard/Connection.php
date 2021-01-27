dcbb3c45c91ef0083a3e2879f3f8eb9e2873340e
Shikhar Tyagi <shikhart98@gmail.com>
2021-01-27 07:27:53 +0530

    fix seller login

--- SellerDashboard/SellerDashboard/Connection.php
+++ SellerDashboard/SellerDashboard/Connection.php
@@ -7,18 +7,18 @@
 
 	$con = mysqli_connect($host, $user, $pass, $db_name);
 
-	if($con){
-		echo "Connection to Database Failed";
-	}
+	// if($con){
+	// 	echo "Connection to Database Failed";
+	// }
 	
-$stmt = "Select * from users_info";
- $query = mysqli_query($conn,$stmt);
-$result = mysqli_num_rows($query);
- if($result>0)
- {
-     while($row = mysqli_fetch_assoc($query))
-    {
-         echo $row['user_first_name']."<br>";
-    } }
+// $stmt = "Select * from users_info";
+//  $query = mysqli_query($conn,$stmt);
+// $result = mysqli_num_rows($query);
+//  if($result>0)
+//  {
+//      while($row = mysqli_fetch_assoc($query))
+//     {
+//          echo $row['user_first_name']."<br>";
+//     } }
 
 ?>
